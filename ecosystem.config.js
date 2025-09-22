f154ccc02def3a14aed6f33f2dfe8aad6ea6919e
Jason van Hattum <jason.vanhattum@fareharbor.com>
2025-09-22 18:19:30 +0200

    chore: chore
    Signed-off-by: Jason van Hattum <jason.vanhattum@fareharbor.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -33,8 +33,7 @@ module.exports = {
       "host": ["rigel.usbx.me"],
       "ref": "origin/feat/bun", // todo: change to main
       "repo": "https://github.com/Jason-vh/kyle",
-      "path": "/home/jasonvh/kyle",
-      "post-deploy": "/home/jasonvh/.bun/bin/bun ci"
+      "path": "/home/jasonvh/kyle"
     }
   }
 };
