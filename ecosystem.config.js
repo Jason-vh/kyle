24fc540684d0fecb3b11b721d972fbc75ff9f68f
Jason van Hattum <jason.vanhattum@fareharbor.com>
2025-09-22 18:28:04 +0200

    chore: chore
    Signed-off-by: Jason van Hattum <jason.vanhattum@fareharbor.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
 
       // Advanced settings
       merge_logs: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     }
   ],
   deploy: {
@@ -34,7 +34,7 @@ module.exports = {
       "ref": "origin/feat/bun", // todo: change to main
       "repo": "https://github.com/Jason-vh/kyle",
       "path": "/home/jasonvh/kyle",
-      "post-deploy": "/home/jasonvh/.bun/bin/bunx pm2 startOrRestart ecosystem.config.js --env production",
+      "post-deploy": "/home/jasonvh/.bun/bin/bun ci && /home/jasonvh/.bun/bin/bunx pm2 startOrRestart ecosystem.config.js --env production",
     }
   }
 };
